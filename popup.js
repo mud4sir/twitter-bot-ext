26160a5cbe17b0216f1203a27b10acb0ceb9207a
mud4sir <mudassirkhan9991@gmail.com>
2023-06-16 22:01:27 +0500

    custom api connection field added,repeating accounts names are made unique, api key saved with reference to windowId in localStorage and styling done

--- popup.js
+++ popup.js
@@ -1,24 +1,17 @@
 // constants
-const accountsUrl =
-  "https://script.googleusercontent.com/macros/echo?user_content_key=fVLBqz0WBJa2fpEmRrtC3xaZQ7nBMX-yc44xvSgH_nbLhcOFmfLPUxuflpES3Prq2YT7QEpoP2PzhkgQNUF8w2bzVqvFF1k5m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMA0DTO7--I9cSGJU1PdSh9B8zxf6AN40Cq5tNYeal_bPk4SH7gtpGImX6-jvriUGUzMSAydGnU8CY6NRsK5hw9P1sMGBIl-ag&lib=Ms6_62q9Tg9tm3p_I1Qc-ptEeF5Y93Lss";
+let accountsUrl;
+const windowId = chrome.windows.WINDOW_ID_CURRENT;
 
-// utils fn
-const getAccounts = async (url) => {
-  try {
-    let loading = true;
-    const response = await fetch(url);
-    const { data } = await response.json();
-    loading = false;
-    return {
-      data,
-      loading,
-    };
-  } catch (err) {
-    console.log(err);
-  }
-};
+document.addEventListener("DOMContentLoaded", async function () {
+  // see if api key exists in local storage
+  await chrome.storage.local.get(`${windowId}`, async (result) => {
+    const apiKeyLocalStorage = await result[`${windowId}`];
+    accountsUrl = apiKeyLocalStorage;
+    if (apiKeyLocalStorage.trim() !== "") {
+      displayMainPage();
+    }
+  });
 
-document.addEventListener("DOMContentLoaded", function () {
   // btns
   const getDataButton = document.getElementById("getDataButton");
   const accountSelect = document.getElementById("accountSelect");
@@ -28,17 +21,112 @@ document.addEventListener("DOMContentLoaded", function () {
   const backButton = document.getElementById("backButton");
   const deleteAccountButton = document.getElementById("deleteAccountButton");
   const enterDelayBtn = document.getElementById("enterDelayBtn");
+  const gotoMainPageBtn = document.getElementById("backMainPageBtn");
+  const connectionBtn = document.getElementById("connectionBtn");
+  const connectSheetBtn = document.getElementById("connectSheetBtn");
 
   // els
   const delayBtnsDiv = document.getElementById("delayBtnMain");
   const accountForm = document.querySelector(".account-form");
-  const accountNameInput = document.getElementById("accountName");
   const minDelayInput = document.getElementById("minDelay");
   const maxDelayInput = document.getElementById("maxDelay");
   const minDelayInputMain = document.getElementById("minDelayMain");
   const maxDelayInputMain = document.getElementById("maxDelayMain");
+  const connectionPage = document.getElementById("connectionPage");
+  const mainPage = document.getElementById("mainPage");
+  const linkToSheet = document.getElementById("linkToSheet");
 
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  // utils fn
+  const getAccounts = async (url) => {
+    let data;
+    let loading = true;
+    let error = false;
+    try {
+      const response = await fetch(url);
+      data = await response.json();
+      loading = false;
+    } catch (err) {
+      error = true;
+      console.log("Error:", err);
+    } finally {
+      return {
+        data: data?.data,
+        loading,
+        error,
+      };
+    }
+  };
+
+  const compare = (a, b) => a === b;
+
+  const displayAccountManagementPage = () => {
+    accountForm.style.display = "block";
+    accountSelect.style.display = "none";
+    accountMgtButton.style.display = "none";
+    getDataButton.style.display = "none";
+    enterDelayBtn.style.display = "none";
+    connectionBtn.style.display = "none";
+  };
+
+  const displayMainPage = () => {
+    connectionPage.style.display = "none";
+    accountForm.style.display = "none";
+    mainPage.style.display = "block";
+    accountSelect.style.display = "block";
+    accountMgtButton.style.display = "block";
+    getDataButton.style.display = "block";
+    enterDelayBtn.style.display = "block";
+    connectionBtn.style.display = "block";
+  };
+
+  const displayConnectionPage = () => {
+    connectionPage.style.display = "block";
+    mainPage.style.display = "none";
+  };
+
+  const attachClickEventListner = (element, callBack) => {
+    element.addEventListener("click", () => {
+      callBack();
+    });
+  };
+
+  function saveDataLocalStorage(key, value, message) {
+    const data = {};
+    data[`${key}`] = value;
+
+    chrome.storage.local.set(data, () => {
+      alert(message);
+    });
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
   // Event Listeners
+
+  connectSheetBtn.addEventListener("click", async () => {
+    const link = linkToSheet.value;
+    if (link.trim() === "") return;
+
+    connectSheetBtn.innerText = "Loading...";
+    const { data: accountsList, error } = await getAccounts(link);
+    connectSheetBtn.innerText = "Save";
+    if (error) {
+      alert("Error occurred, enter a valid link");
+      linkToSheet.value = "";
+      return;
+    }
+    // params -> saveDataLocalStorage (key, value, message)
+    saveDataLocalStorage(windowId, link, "Api key saved successfully");
+    displayMainPage();
+    linkToSheet.value = "";
+  });
+
+  attachClickEventListner(connectionBtn, displayConnectionPage);
+  attachClickEventListner(gotoMainPageBtn, displayMainPage);
 
   enterDelayBtn.addEventListener("click", () => {
     if (
@@ -51,7 +139,7 @@ document.addEventListener("DOMContentLoaded", function () {
       enterDelayBtn.innerText = "Enter Delay";
       delayBtnsDiv.style.display = "none";
 
-      // TODO: reset input fn
+      // TODO: reset input min/max fn
       minDelayInputMain.value = "";
       maxDelayInputMain.value = "";
     }
@@ -73,8 +161,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var minDelay = null;
     var maxDelay = null;
 
-    // TODO: take data from main page's min/max input otherwise go with stored values.
-
     minDelay = parseInt(minDelayInputMain.value) || parseInt(minValue);
     maxDelay = parseInt(maxDelayInputMain.value) || parseInt(maxValue);
 
@@ -83,9 +169,7 @@ document.addEventListener("DOMContentLoaded", function () {
       return;
     }
 
-    let apiURL =
-      "https://script.google.com/macros/s/AKfycbz0t2NkfYmlT_DjVisvRggrSO6OjZ04c_PDApr5AnyeSpiwGaq0vH9yKx2vfkdJlNyK/exec?accName=" +
-      selectedAccount;
+    let apiURL = accountsUrl + "?accName=" + selectedAccount;
     if (customDelay) {
       apiURL += "&minDelay=" + minDelay + "&maxDelay=" + maxDelay;
     }
@@ -99,29 +183,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  accountMgtButton.addEventListener("click", async function () {
-    // TODO: display account managemnet page fn
-    accountForm.style.display = "block";
-    accountSelect.style.display = "none";
-    accountMgtButton.style.display = "none";
-    getDataButton.style.display = "none";
-    enterDelayBtn.style.display = "none";
+  attachClickEventListner(accountMgtButton, async function () {
+    displayAccountManagementPage();
+
+    let savedAccountsList;
+    chrome.storage.local.get(["accounts"], function (result) {
+      savedAccountsList = result.accounts || [];
+    });
 
     accountAdd.innerHTML =
       '<option disabled selected value="">Loading accounts...</option>';
 
     const { data: accountsList } = await getAccounts(accountsUrl);
-    accountsList.shift();
-    addSelectedAccount(accountsList);
+    accountsList?.shift();
+    addSelectedAccount(accountsList, savedAccountsList);
   });
 
-  saveAccountButton.addEventListener("click", function () {
+  attachClickEventListner(saveAccountButton, function () {
     const accountName = accountAdd.value.trim();
     var minDelay = parseInt(minDelayInput.value);
     var maxDelay = parseInt(maxDelayInput.value);
 
     if (accountName === "") {
-      alert("Please enter an account name.");
+      alert("Please select an account.");
       return;
     }
 
@@ -138,7 +222,6 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.local.get(["accounts"], function (result) {
       var accounts = result.accounts || [];
 
-      // Check if the account already exists
       var existingAccount = accounts.find(function (account) {
         return account.name === accountName;
       });
@@ -154,37 +237,27 @@ document.addEventListener("DOMContentLoaded", function () {
         maxDelay: maxDelay,
       });
 
-      chrome.storage.local.set({ accounts: accounts }, function () {
-        alert("Account profile saved successfully.");
-        minDelayInput.value = "";
-        maxDelayInput.value = "";
+      saveDataLocalStorage(
+        "accounts",
+        accounts,
+        "Account profile saved successfully"
+      );
 
-        // TODO: display main page fn
-        accountForm.style.display = "none";
-        accountSelect.style.display = "block";
-        accountMgtButton.style.display = "block";
-        getDataButton.style.display = "block";
-        enterDelayBtn.style.display = "block";
-
-        updateAccountSelect(accounts);
-      });
+      minDelayInput.value = "";
+      maxDelayInput.value = "";
+      displayMainPage();
+      updateAccountSelect(accounts);
     });
   });
 
-  backButton.addEventListener("click", function () {
-    accountForm.style.display = "none";
-    accountSelect.style.display = "block";
-    accountMgtButton.style.display = "block";
-    getDataButton.style.display = "block";
-    enterDelayBtn.style.display = "block";
-  });
+  attachClickEventListner(backButton, displayMainPage);
 
   chrome.storage.local.get(["accounts"], function (result) {
     var accounts = result.accounts || [];
     updateAccountSelect(accounts);
   });
 
-  deleteAccountButton.addEventListener("click", function () {
+  attachClickEventListner(deleteAccountButton, function () {
     const accountName = accountAdd.value.trim();
 
     if (accountName === "") {
@@ -203,15 +276,11 @@ document.addEventListener("DOMContentLoaded", function () {
         accounts.splice(index, 1);
         chrome.storage.local.set({ accounts: accounts }, function () {
           alert("Account profile deleted successfully.");
+          // TODO: reset input min/max value fn
           minDelayInput.value = "";
           maxDelayInput.value = "";
 
-          // TODO: display main page fn
-          accountForm.style.display = "none";
-          accountSelect.style.display = "block";
-          accountMgtButton.style.display = "block";
-          getDataButton.style.display = "block";
-          enterDelayBtn.style.display = "block";
+          displayMainPage();
 
           updateAccountSelect(accounts);
         });
@@ -222,10 +291,13 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   async function updateAccountSelect(accounts) {
+    console.log("accounts", accounts);
     accountSelect.innerHTML =
       '<option disabled selected value="">Select an account</option>';
     accounts.forEach(function (account) {
-      var option = document.createElement("option");
+      // TODO: this functionality can be exported to a function
+      // it's repeatitive
+      const option = document.createElement("option");
       option.value = account?.name || account["account name"];
       option.textContent = account?.name || account["account name"];
       option.dataset.maxValue = account?.maxDelay;
@@ -234,16 +306,72 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  async function addSelectedAccount(accounts) {
-    accountAdd.innerHTML =
-      '<option disabled selected value="">Select an account</option>';
-    accounts.forEach(function (account) {
-      var option = document.createElement("option");
-      option.value = account?.name || account["account name"];
-      option.textContent = account?.name || account["account name"];
-      option.dataset.maxValue = account?.maxDelay;
-      option.dataset.minValue = account?.minDelay;
-      accountAdd.appendChild(option);
-    });
+  async function addSelectedAccount(accounts, saveAccounts) {
+    // TODO: refactoring needs to be here
+
+    const uniqueUnsavedAccounts = Object.values(
+      accounts.reduce((acc, obj) => {
+        const name = obj["account name"].trim();
+        if (!acc[name]) {
+          acc[name] = obj;
+        }
+        return acc;
+      }, {})
+    );
+
+    const unsavedAccounts = uniqueUnsavedAccounts.filter(
+      (item1) =>
+        !saveAccounts.some((item2) =>
+          compare(item1["account name"].trim(), item2?.name.trim())
+        )
+    );
+
+    accountAdd.innerHTML = `<option disabled selected value="">Select an account</option>`;
+    const optgroup = document.createElement("optgroup");
+    optgroup.label = "----- Available Accounts ------";
+    accountAdd.appendChild(optgroup);
+    console.log("unsavedAccounts", unsavedAccounts);
+    if (unsavedAccounts.length <= 0) {
+      const option = document.createElement("option");
+      option.textContent = "no accounts available";
+      option.disabled = true;
+      optgroup.appendChild(option);
+    }
+    if (unsavedAccounts.length > 0) {
+      unsavedAccounts.forEach(function (account) {
+        const option = document.createElement("option");
+        option.value = account?.name || account["account name"];
+        option.textContent = account?.name || account["account name"];
+        option.dataset.maxValue = account?.maxDelay;
+        option.dataset.minValue = account?.minDelay;
+        optgroup.appendChild(option);
+      });
+    }
+
+    if (saveAccounts.length <= 0) {
+      const optgroup = document.createElement("optgroup");
+      optgroup.label = "----- Added Accounts ------";
+      accountAdd.appendChild(optgroup);
+
+      const option = document.createElement("option");
+      option.textContent = "no accounts available";
+      option.disabled = true;
+      optgroup.appendChild(option);
+    }
+
+    if (saveAccounts.length > 0) {
+      const optgroup = document.createElement("optgroup");
+      optgroup.label = "----- Added Accounts ------";
+      accountAdd.appendChild(optgroup);
+
+      saveAccounts.forEach(function (account) {
+        const option = document.createElement("option");
+        option.value = account?.name || account["account name"];
+        option.textContent = account?.name || account["account name"];
+        option.dataset.maxValue = account?.maxDelay;
+        option.dataset.minValue = account?.minDelay;
+        optgroup.appendChild(option);
+      });
+    }
   }
 });
